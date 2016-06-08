07498d2541cfd0e088643045c1aa8b71f23d355b
Shad Ansari <shad.ansari@hpe.com>
2016-06-08 14:32:11 -0700

    Fix checkmk to issue garbage ovs-vsctl cmds.
    TG-535
    
    Change-Id: Ic7a7b78573e757f50409600b6e037dfb5d313f2b
    Signed-off-by: Shad Ansari <shad.ansari@hpe.com>

--- yocto/openswitch/meta-distro-openswitch/recipes-ops/mgmt/ops-checkmk-agent/checkmk-agent.sh
+++ yocto/openswitch/meta-distro-openswitch/recipes-ops/mgmt/ops-checkmk-agent/checkmk-agent.sh
@@ -219,24 +219,7 @@ cat /proc/uptime
 
 # New variant: Information about speed and state in one section
 echo '<<<lnx_if:sep(58)>>>'
-for i in $(ovs-vsctl list Interface | grep name | cut -c 24- | sed 's/"$//') ; do
-    if [ $(ovs-vsctl get Interface $i admin_state | grep "up") ]
-    then
-        echo if$i: $(ovs-vsctl get Interface $i statistics:rx_bytes) $(ovs-vsctl get Interface $i statistics:rx_packets) $(ovs-vsctl get Interface $i statistics:rx_crc_err) $(ovs-vsctl get Interface $i statistics:rx_dropped) 0 0 0 0 $(ovs-vsctl get Interface $i statistics:tx_bytes) $(ovs-vsctl get Interface $i statistics:rx_packets) $(ovs-vsctl get Interface $i statistics:rx_crc_err) $(ovs-vsctl get Interface $i statistics:rx_dropped) 0 $(ovs-vsctl get Interface $i statistics:collisions) 0 0
-    fi
-done
 sed 1,2d /proc/net/dev
-for i in $(ovs-vsctl list Interface | grep name | cut -c 24- | sed 's/"$//') ; do
-    if [ $(ovs-vsctl get Interface $i admin_state | grep "up") ]
-    then
-        echo [if$i]
-        echo -e "\tSpeed: $(ovs-vsctl get Interface $i link_speed | sed 's/.\{6\}$//')Mb/s"
-        echo -e "\tDuplex: $(ovs-vsctl get Interface $i duplex | sed -e "s/\b\(.\)/\u\1/g")"
-        echo -e "\tAuto-negotiation: "
-        echo -e "\tLink detected: $(ovs-vsctl get Interface $i link_state | sed -e 's/up/yes/g' -e 's/down/no/g')"
-        echo -e "\tAddress: $(ovs-vsctl get Interface $i mac_in_use | sed -e 's/"//g')"
-    fi
-done
 if type ethtool > /dev/null
 then
     for eth in $(sed -e 1,2d < /proc/net/dev | cut -d':' -f1 | sort)
